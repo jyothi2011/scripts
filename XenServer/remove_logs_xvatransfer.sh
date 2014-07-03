3a743de8b02a122447b285370001c51b094a04fa
Ricardo Martins <ricardo.macedo@yahoo.com.br>
2014-07-03 14:05:54 -0300

    Update remove_logs_xvatransfer.sh

--- XenServer/remove_logs_xvatransfer.sh
+++ XenServer/remove_logs_xvatransfer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Apagar logs antigos do script de envio dos xva's para o S3
+# Delete old logs from xvatransfer_all.sh
 # Ricardo Martins - http://www.ricardomartins.com.br
 for logs in `find /var/log/backupxen/xvatransfer/ -mtime +7`; do
 rm -rf ${logs}
