5968c57a23fbf516ae6fed8d4f8a26d8f63c88d3
Ricardo Martins <ricardo.macedo@yahoo.com.br>
2014-07-03 14:05:20 -0300

    Update remove_logs_snapshot.sh

--- XenServer/remove_logs_snapshot.sh
+++ XenServer/remove_logs_snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Apagar logs antigos do script de cria��o de snapshots
+# Delete old logs from snapshot_transfer_s3.sh
 # Ricardo Martins - http://www.ricardomartins.com.br
 for logs in `find /var/log/backupxen/snapshot/ -mtime +7`; do
 rm -rf ${logs}
