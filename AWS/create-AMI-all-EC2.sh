24fe8e69aeee982ed7d7af4b6daa3016a9a98243
Ricardo Martins <ricmmart@gmail.com>
2014-08-12 15:57:19 -0300

    Shell script to make image on EC2 instances

--- AWS/create-AMI-all-EC2.sh
+++ AWS/create-AMI-all-EC2.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-
+# v.01 - 12/08/2014
+# Ricardo Martins  - contato@ricardomartins.com.br
 set -x
+source /root/.bashrc
 export EC2_HOME='/opt/aws/apitools/ec2'
 export EC2_BIN=$EC2_HOME/bin
 export PATH=$PATH:$EC2_BIN
-export AWS_CREDENTIAL_FILE=/root/.ec2/snapshot.cred
-export EC2_PRIVATE_KEY=/root/.ec2/snapshot-pkcs8.key
-export EC2_CERT=/root/.ec2/snapshot.crt
 export AWS_ELB_HOME=/opt/aws/apitools/elb
 export JAVA_HOME=/usr/lib/jvm/jre
+SCRIPT_DIR=/root/scripts
+FEC2DIN_DIR=/root/scripts
+FEC2DIN=$FEC2DIN_DIR/fec2din
 
-rm -rf output*.txt
 
-ec2-describe-instances  --filter tag:Name="PDAM*" --filter "instance-state-name=running" > output.txt
-sed -e '/^INSTANCE/d' -e '/^BLOCK/d' -e '/^RESERVATION/d' output.txt > output2.txt
-sed '/Description/d' ./output2.txt > output3.txt
-awk '{ print $3,$5 }' output3.txt > output4.txt
-awk -v date="$(date +"%Y-%m-%d")" '{print $1,"--name",$2,"-",date,"--no-reboot"}' output4.txt | gawk '{print $1,$2,$3 $4 $5,$6 $7}' > output5.txt
-awk '{ print $1,$3 }' output5.txt
+cd $SCRIPT_DIR
+rm -rf $SCRIPT_DIR/instances*
 
-cat output5.txt | xargs -L 1 ec2-create-image
+/usr/bin/python2.6 $FEC2DIN > $SCRIPT_DIR/instances_full
+cat $SCRIPT_DIR/instances_full | sed -e '/^AMI/d' -e '/^Type/d' -e '/^Public/d' -e '/^PrivIP/d' -e '/^PubKey/d' -e '/^Days/d' -e '/^AZ/d' -e '/^Disks/d' -e '/^Groups/d' -e '/dev/d' -e '/^Group/d' |  awk '{ if($2) printf("%s ", $2); else print ""; }' > $SCRIPT_DIR/instances
+cat $SCRIPT_DIR/instances | awk -v date="$(date +"%d%m%Y")" '{print $1,"--name",$2"_"date,"--no-reboot"}' > $SCRIPT_DIR/ami-input
+cat $SCRIPT_DIR/ami-input | xargs -L 1 ec2-create-image
