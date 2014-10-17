557db6694e60937e71d7d1ff981de7ef7effeadc
Ricardo Martins <ricmmart@gmail.com>
2014-10-17 12:10:32 -0300

    Removed unnecessary block

--- System Management/check-updates-debian.sh
+++ System Management/check-updates-debian.sh
@@ -12,10 +12,6 @@ echo "==============================================================" > $LOG 2>&
 echo $(date +%d.%m.%Y\ %R:%S\ ) - "General updates available:" >> $LOG 2>&1
 echo "$UPDATES" >> $LOG 2>&1
 echo "" >> $LOG 2>&1
-echo "==============================================================" >> $LOG 2>&1
-echo $(date +%d.%m.%Y\ %R:%S\ ) - "List of security updates available:" >> $LOG 2>&1
-echo "$SECURITY_UPDATES" >> $LOG 2>&1
-echo "" >> $LOG 2>&1
 mail -s "Updates for $(hostname)" contato@ricardomartins.com.br < $LOG
 
 for logs in `find $LOGDIR -mtime +7`; do
